2ef8aa52256d27e852bb58bdd6e97cdd921b13a2
Nasibb <nasibgi@code.edu.az>
2023-10-20 17:14:59 +0400

    Start

---
+++ MassivePractise/Program.cs
@@ -0,0 +1,56 @@
+﻿#region Introduction
+
+//int[] numbers = { 29, 3, 85, 92, 123, 4, 0, 1, 1, 2 };
+//foreach (int num in numbers)
+//{
+//    Console.WriteLine(num);
+//}
+
+//    Console.WriteLine(numbers[0]);
+
+
+//int[] numbers = { 29, 3, 85, 92, 123, 4, 0, 1, 1, 2 };
+//Console.WriteLine(numbers[0]);
+//Console.WriteLine(numbers[numbers.Length-1]=1024);
+
+//bool[] a = {true, false};
+//Console.WriteLine(a[1]);
+
+//string[] names = { "N", "oku", "am" };
+//Console.WriteLine(names[0]="Nasib");
+
+#endregion
+
+
+#region Task for yourself
+
+int[] num = { 130, 0, 1, 45, 85, 129 };
+
+//1.Birinci və Sonuncu massivin cəmini tapın.
+//Console.WriteLine(num[0] + num[num.Length-1]);
+
+
+//2. Massiv 0 ve 85 goster, hem de topla.
+//Console.WriteLine(num[1]);
+//Console.WriteLine(num[4]);
+//Console.WriteLine(num[1] + num[4]);
+
+
+//3. Sonuncusun ve 1 massivini, hem de 0ci indexi deyish ve goster.
+//num[num.Length-1] = 123;
+//num[2]=1024;
+//num[0] = 4;
+//Console.WriteLine(num[num.Length-1]);
+//Console.WriteLine(num[2]);
+//Console.WriteLine(num[0]);
+
+//4. Butun massivleri ve indexleri ekranda eks eletdir.
+for (int i = 0; i < num.Length; i++)
+    Console.WriteLine(num[i]);
+Console.WriteLine(i);
+
+//5.Indexleri goster
+
+#endregion
+
+
